d9a83e54bc7c6587c9cb21eb747ddce1f1c5fb99
tatsupiko <identity2022@outlook.jp>
2022-06-13 19:36:59 +0900

    プレイヤーの挙動の改善 メインウェポンの正常稼働化

--- Assets/My folder/Playercontroller.cs
+++ Assets/My folder/Playercontroller.cs
@@ -4,35 +4,54 @@ using UnityEngine;
 
 public class Playercontroller : MonoBehaviour
 {
+    [SerializeField] float C_interval = 1f;
+    [SerializeField] float n;
     [SerializeField] float _macineSpeed;
     Rigidbody2D _rb;
-    [SerializeField] GameObject _MainShooter;
+    GameObject _MainShooter;
     void Start()
     {
         Cursor.visible = false;
+        n = C_interval;
         _rb = GetComponent<Rigidbody2D>();
     }
     // Update is called once per frame
+
+    [SerializeField] ShootManager _bulletC = default;
+    [SerializeField] Transform muzzle_C = default;
     void FixedUpdate()
     {       
         if (Input.GetButton("Fire1"))
         {   
-            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); //空間でのマウス位置の計算
+            //空間でのマウス位置の計算
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
             Vector3 vec = mousePosition - transform.position;
             _rb.velocity = Vector2.zero;
             _rb.AddForce(vec.normalized * _macineSpeed, ForceMode2D.Impulse);
-            //Debug.Log(mousePosition);
-            //mousePosition.z = 0;
-            //this.transform.position = mousePosition;
 
-            _MainShooter.GetComponent<MainManager>();
+            n += Time.deltaTime;
+            //弾の発射間隔
+            if (n >= C_interval)
+            {
+                n = 0;
+
+                //main weapon
+                var bul1 = Instantiate(_bulletC, muzzle_C.position, this.transform.rotation);
+                bul1.Direction = Vector3.up;
+                bul1.transform.position = muzzle_C.position;
+            }
+        }
+        else
+        {
+            _rb.velocity = Vector3.zero;
         }
 
         //クリックを離したら、機体が止まる(止まらない)
-        if(Input.GetMouseButtonUp(0))
-            {
-                _rb.velocity = Vector2.zero;
-            }
+        //if(Input.GetMouseButtonUp(0))
+        //{
+        //    _rb.velocity = Vector3.zero;
+        //    Debug.Log("a");
+        //}
     }
 }
     
