1950dc9292bf8ce2eb8724efd21dd1353836ac8f
tatsupiko <identity2022@outlook.jp>
2022-06-13 20:17:08 +0900

    ボスコントローラーの追加

--- Assets/My folder/BossController.cs
+++ Assets/My folder/BossController.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BossController : MonoBehaviour
 {
+    [SerializeField] string _weapon = "weapon";
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +15,11 @@ public class BossController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
     {
         
     }
